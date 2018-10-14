bffe902f1c7116c63586babcb2f88474f64e64ae
Peter Kreppein <pmkreppein@gmail.com>
2018-10-14 22:20:09 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,29 +1,26 @@
 class Doctor
   attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
-   def self.all 
+
+  def self.all
     @@all
   end
-  
-  def new_appointment(date, patient)
-   Appointment.new(patient, self, date)
-   
-  end
-  
-  def appointments
-        Appointment.all.select { |appt| appt.doctor == self }
 
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
   end
-  
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
   def patients
     appointments.map(&:patient)
   end
-  
- 
 end
