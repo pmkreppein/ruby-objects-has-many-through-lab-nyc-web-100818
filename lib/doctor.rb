b21c4ae1aed2ea13ed70084f21ef0088e3e94650
Peter Kreppein <pmkreppein@gmail.com>
2018-10-14 22:19:48 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def appointments
-        Appointment.all.select { |appointment| appointment.doctor == self }
+        Appointment.all.select { |appt| appt.doctor == self }
 
   end
   
